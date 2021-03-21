165474180b5f3d60d79f685eb546dd011e9688e9
Tothadam80 <toth.adam80@gmail.com>
2021-03-21 21:54:41 +0100

    jsgyak create

---
+++ hajosproba/hajosproba/wwwroot/jsgyak.js
@@ -0,0 +1,21 @@
+﻿var faktorialis = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * 1;
+    }
+    return er;
+
+    
+}
+
+var faktorialisR = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n *faktorialisR(n-1)
+    }
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log('${i} : ${faktorialisR}')
+}
