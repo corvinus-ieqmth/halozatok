655d28afd3583f2611564b32a48cf77717f3e4d8
Tothadam80 <toth.adam80@gmail.com>
2021-03-27 23:35:54 +0100

    hetedik.js, questions.json create

---
+++ hajosproba/hajosproba/wwwroot/hetedik.js
@@ -0,0 +1,55 @@
+﻿var kerdesek;
+var kerdesSorszam = 0;
+
+ function letöltés() {
+
+    fetch('/questions.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    kerdesek = d;
+    KerdesMegjelenites(0)
+}
+
+
+window.onload = function () {
+    
+    letöltés();
+
+    document.getElementById("vissza").onclick = function () {
+        kerdesSorszam--;
+        if (kerdesSorszam == -1) {
+            KerdesMegjelenites(2);
+        }
+    }
+
+    document.getElementById("előre").onclick = function () {
+        kerdesSorszam++;
+        if (kerdesSorszam == 3) {
+            KerdesMegjelenites(0)
+        }
+    }
+    
+}
+
+function KerdesMegjelenites(kerdesSzam) {
+    let kerdes_szoveg = document.getElementById("kérdés_szöveg")
+    let kep = document.getElementById("kép1");
+    let valasz1 = document.getElementById("válasz1");
+    let valasz2 = document.getElementById("válasz2");
+    let valasz3 = document.getElementById("válasz3");
+
+    kerdes_szoveg.innerHTML = kerdesek[kerdesSzam].questionText
+    kep.src = "https://szoft1.comeback.hu/hajo/" + kerdesek[kerdesSzam].image
+    valasz1.innerText = kerdesek[kerdesSzam].answer1
+    valasz2.innerText = kerdesek[kerdesSzam].answer2
+    valasz3.innerText = kerdesek[kerdesSzam].answer3
+
+
+
+}
