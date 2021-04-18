c9dc35098baf5a75bf84e4fa7ebad585936b116c
Tothadam80 <toth.adam80@gmail.com>
2021-04-18 18:34:26 +0200

    api létrehoázsa, adatbázis kapcsolat létrehozása

--- hajosproba/hajosproba/Startup.cs
+++ hajosproba/hajosproba/Startup.cs
@@ -16,6 +16,7 @@ namespace hajosproba
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,11 @@ namespace hajosproba
                 });
             });
             */
+
+            app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
         }
     }
 }
