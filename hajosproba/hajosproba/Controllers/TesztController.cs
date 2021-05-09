de2a35e373952a10bfd743174bec373e8ff2f103
Tothadam80 <toth.adam80@gmail.com>
2021-05-09 21:25:11 +0200

    tizedik.js

--- hajosproba/hajosproba/Controllers/TesztController.cs
+++ hajosproba/hajosproba/Controllers/TesztController.cs
@@ -1,3 +1,65 @@
+
+﻿using hajosproba.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hajosproba.Controllers
+{
+
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/countNew")]
+        public int M1()
+        {
+            HajostesztContext context = new HajostesztContext();
+            int kerdesekSzama = context.Questions.Count();
+            return kerdesekSzama;
+        }
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult M3()
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdések = from x in context.Questions select x.QuestionText;
+
+            return new JsonResult(kérdések);
+        }
+        [HttpGet]
+        [Route("questions/{sorszam}")]
+        public ActionResult M2(int sorszam)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kerdes = (from x in context.Questions
+                          where x.QuestionId == sorszam
+                          select x).FirstOrDefault();
+            if (kerdes == null)
+            {
+                return BadRequest("Nincs ilyen sorszámú kérdés");
+            }
+
+            return new JsonResult(kerdes);
+        }
+
+
+        //az elsővel megegyezik
+        [HttpGet]
+        [Route("questions/count")]
+        public int M4() //Tetszőleges metódusnév
+        {
+            HajostesztContext context = new HajostesztContext();
+            int kérdésekSzáma = context.Questions.Count();
+
+            return kérdésekSzáma;
+        }
+    }
+    /*
 ﻿using hajosproba.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,15 +84,23 @@ namespace hajosproba.Controllers
 
             return new JsonResult(kérdések);
         }
-
-        [HttpGet]
-        [Route("questions/count")]
-        public int M4() //Tetszőleges metódusnév
+        public ActionResult M2(int sorszam)
         {
-            HajostesztContext context = new HajostesztContext();
-            int kérdésekSzáma = context.Questions.Count();
+            HajosteszContext context = new HajosteszContext();
+            var kerdes = (from x in context.Questions
+                          where x.QuestionId == sorszam
+                          select x).FirstOrDefault();
+            if (kerdes == null)
+            {
+                return BadRequest("Nincs ilyen sorszámú kérdés");
+            }
 
-            return kérdésekSzáma;
+            return new JsonResult(kerdes);
         }
+    
+>>>>>>> d072c6cb14af56869769a4ffbde7afc9eebe0f0f
     }
+}*/
 }
+
+
---
+++ hajosproba/hajosproba/wwwroot/tizedik.js
@@ -0,0 +1,45 @@
+﻿var hotList = [];
+var questionsInHotList = 3;
+var displayedQuestion;
+var numberOfQuestions;
+var nextQuestion = 1; 
+
+function init() {
+    for (var i = 0; i < questionsInHotList; i++) {
+        let q = {
+            question: {},
+            goodAnswers: 0
+        }
+        hotList[i] = q;
+    }
+
+
+    for (var i = 0; i < questionsInHotList; i++) {
+        kérdésBetöltés(nextQuestion, i);
+        nextQuestion++;
+    }
+}
+
+function kérdésBetöltés(questionNumber, destination) {
+    fetch(`/questions/${questionNumber}`)
+        .then(
+            result => {
+                if (!result.ok) {
+                    console.error(`Hibás letöltés: ${response.status}`)
+                }
+                else {
+                    return result.json()
+                }
+            }
+        )
+        .then(
+            q => {
+                hotList[destination].question = q;
+                hotList[destination].goodAnswers = 0;
+                console.log(`A ${questionNumber}. kérdés letöltve a hot list ${destination}. helyére`)
+                if (displayedQuestion == undefined && destination == 0) {
+                    displayedQuestion = 0;
+                    kérdésMegjelenítés();
+                }
+        );
+}
